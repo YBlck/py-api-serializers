723042cee65cd315005583fbbf7abefa87e77b44
Yura Chornyi <chorny.yura@gmail.com>
2025-03-23 04:08:04 +0200

    feat: add routes

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,25 @@
-# write urls here
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from cinema.views import (
+    MovieSessionViewSet,
+    ActorViewSet,
+    MovieViewSet,
+    CinemaHallViewSet,
+    GenreViewSet
+)
+
+app_name = "cinema"
+
+router = DefaultRouter()
+
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("movies", MovieViewSet)
+router.register("movie_sessions", MovieSessionViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
